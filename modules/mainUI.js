b9375b4b66a7839bf4c73bc81951b1033511890a
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-02 17:51:42 -0500

    Add dynamic creation and styling for stats display

--- modules/mainUI.js
+++ modules/mainUI.js
@@ -1,4 +1,5 @@
 let mainUI = document.createElement('div')
+let ageBox = document.createElement('div')
 let ageBtn = document.createElement('div')
 
 
@@ -15,7 +16,40 @@ let midUI = document.createElement('div')
 //bottomUI 
 let bottomUI = document.createElement('div')
 let bottomLeft = document.createElement('div')
+
+let bottomLeftOne = document.createElement('div')
+let bottomLeftTwo = document.createElement('div')
+
 let bottomRight = document.createElement('div')
+let bottomRightOne = document.createElement('div')
+let bottomRightTwo = document.createElement('div')
+
+
+
+let statsMain = document.createElement('div')
+let stats = document.createElement('div')
+
+
+//stats
+
+let happinessDiv = document.createElement('div')
+let healthDiv = document.createElement('div')
+let smartsDiv = document.createElement('div')
+let looksDiv = document.createElement('div')
+// ----------------------------------------------------
+let happiness = document.createElement('h2')
+let health = document.createElement('h2')
+let smarts = document.createElement('h2')
+let looks = document.createElement('h2')
+// ----------------------------------------------------
+
+let happinessBar = document.createElement('div')
+let healthBar = document.createElement('div')
+let smartsBar = document.createElement('div')
+let looksBar = document.createElement('div')
+
+
+// ----------------------------------------------------
 
 
 let lifeStage = document.createElement('div')
@@ -42,11 +76,17 @@ userInterface()
 
 
 function userInterface(){
-	ageBtn.textContent = "+\nAge"
+
 	ageBtn.classList.add('ageBtn')
+	ageBtn.textContent = `+\n Age`
+
+	ageBox.classList.add('ageBox')
+	ageBox.appendChild(ageBtn)
+
+
 	mainUI.append(topUI,midUI,bottomUI)
 
-	ageBtn.addEventListener('click',()=>{
+	ageBox.addEventListener('click',()=>{
 		console.log('You\'re 1 year older')
 	})
 	
@@ -67,6 +107,14 @@ function bottomUIfunc(){
 	bottomLeft.classList.add('bottomLeft')
 	bottomRight.classList.add('bottomRight')
 
+
+	bottomLeftOne.classList.add("bottomLeftOne")
+	bottomLeftTwo.classList.add("bottomLeftTwo")
+	bottomRightOne.classList.add("bottomRightOne")
+	bottomRightTwo.classList.add("bottomRightTwo")
+
+
+
 	lifeStage.classList.add('lifeStage')
 	assets.classList.add('assets')
 	relationships.classList.add('relationships')
@@ -74,18 +122,66 @@ function bottomUIfunc(){
 	
 	options.classList.add('options')
 
+	statsMain.classList.add('statsMain')
 
-	
+	stats.classList.add('stats')
+
+
+
 	bottomUI.classList.add('bottomUI')
 
 
 
-	bottomLeft.append(lifeStage, assets)
-	bottomRight.append(relationships, activities)
+	happinessDiv.setAttribute("class","statBox happinessDiv")
+	healthDiv.setAttribute("class","statBox healthDiv")
+	smartsDiv.setAttribute("class","statBox smartsDiv")
+	looksDiv.setAttribute("class","statBox looksDiv")
+	// ----------------------------------------------------
+	happiness.setAttribute("class","statText happiness")
+	health.setAttribute("class","statText health")
+	smarts.setAttribute("class","statText smarts")
+	looks.setAttribute("class","statText looks")
+
+	happiness.textContent = "Happiness 😊"
+	health.textContent = "Health ❤️"
+	smarts.textContent = "Smarts 🧠"
+	looks.textContent = "Looks 😎"
+
+
+	// ----------------------------------------------------
+	happinessBar.setAttribute("class","statBar")
+	healthBar.setAttribute("class","statBar")
+	smartsBar.setAttribute("class","statBar")
+	looksBar.setAttribute("class","statBar")
+
+
+
+
+
+	bottomLeftOne.append(lifeStage)
+	bottomLeftTwo.append(assets)
+	bottomRightOne.append(relationships)
+	bottomRightTwo.append(activities)
+
+	bottomLeft.append(bottomLeftOne, bottomLeftTwo)
+	bottomRight.append(bottomRightOne, bottomRightTwo)
+
 	
-	options.append(bottomLeft, ageBtn, bottomRight)
+	options.append(bottomLeft, ageBox, bottomRight)
+
 	
-	bottomUI.append(options)
+	stats.append(happinessDiv, healthDiv, smartsDiv, looksDiv)
+	
+	statsMain.append(stats)
+
+	happinessDiv.append(happiness, happinessBar)
+	healthDiv.append(health, healthBar)
+	smartsDiv.append(smarts, smartsBar)
+	looksDiv.append(looks, looksBar)
+
+	bottomUI.append(options,statsMain)
+
+	
 
 }
 
