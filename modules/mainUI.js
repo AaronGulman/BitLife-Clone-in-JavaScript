83450f1a45a59a5cbfee738a1e3d4a56e540d766
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-01 20:43:37 -0500

    Rename actionPanel to helloScreen

--- modules/mainUI.js
+++ modules/mainUI.js
@@ -1,13 +1,13 @@
-export function actionPanel(){
+export function helloScreen(){
 let main = document.querySelector('#main')
-let actionPanel = document.createElement('div')
+let helloScreen = document.createElement('div')
 let playerName = document.createElement('h1')
 
 
 playerName.classList.add('pName')
-actionPanel.classList.add('actionPanel')
-actionPanel.appendChild(playerName)
-main.appendChild(actionPanel)
+helloScreen.classList.add('helloScreen')
+helloScreen.appendChild(playerName)
+main.appendChild(helloScreen)
 
 
 }
