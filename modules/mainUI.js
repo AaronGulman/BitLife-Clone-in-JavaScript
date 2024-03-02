8d690a5a9c54dda30c7592bf778a77b0f5b25dc3
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-02 16:28:46 -0500

    Refactor: Organize UI creation and functionality into separate functions

--- modules/mainUI.js
+++ modules/mainUI.js
@@ -1,40 +1,67 @@
-export let mainUI = document.createElement('div')
+let mainUI = document.createElement('div')
+let ageBtn = document.createElement('div')
+
+//topUI
+
+
+//midUI
+let midUI = document.createElement('div')
+
+
+
+//bottomUI part:
+let bottomUI = document.createElement('div')
+let options = document.createElement('div')
 
 
 export function helloScreen(){
 let main = document.querySelector('#main')
-
-
 mainUI.classList.add('mainUI')
-
-
 main.appendChild(mainUI)
 
+
+//functions
+topUIfunc()
+midUIfunc()
+bottomUIfunc()
 userInterface()
+
 
 }
 
 
- function userInterface(){
-	let topUI = document.createElement('div')
-	let midUI = document.createElement('div')
-	let bottomUI = document.createElement('div')
-	
-	
-	let ageBtn = document.createElement('div')
-	
+function userInterface(){
 	ageBtn.textContent = "+\nAge"
-
-
 	ageBtn.classList.add('ageBtn')
-	topUI.classList.add('topUI')
-	midUI.classList.add('midUI')
-	bottomUI.classList.add('bottomUI')
-
-
-
-
-	bottomUI.append(ageBtn)
 	mainUI.append(topUI,midUI,bottomUI)
+
+	ageBtn.addEventListener('click',()=>{
+		console.log('You\'re 1 year older')
+	})
+	
+}
+
+function topUIfunc(){
+	topUI.classList.add('topUI')
+}
+
+
+function midUIfunc(){
+	midUI.classList.add('midUI')
+
+}
+
+function bottomUIfunc(){
+	options.classList.add('options')
+	bottomUI.classList.add('bottomUI')
+	
+	options.append(ageBtn)
+	
+	bottomUI.append(options)
+
+}
+
+function ageUp(age){
+	age++;
 	
 }
