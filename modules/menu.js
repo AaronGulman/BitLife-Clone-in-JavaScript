9543569a4a965f5bef754c7dfd4c96898f505a86
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-03-24 20:37:20 -0500

    Add a proper class to menu.js and append it to <main>

--- modules/menu.js
+++ modules/menu.js
@@ -1,7 +1,11 @@
+let main = document.querySelector('#main')
+
+
 export function toMenu(){
 	let menu = document.createElement('div')
-	menu.classList.add('mainUI')
-	document.body.append(menu)
-	console.log('clicked')
+	menu.classList.add('menuUI')
+	main.append(menu)
+	console.log('<MENU> clicked')
+
 }
 
