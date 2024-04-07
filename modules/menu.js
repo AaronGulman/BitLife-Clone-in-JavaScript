9b0fb1174b1b47336cff00b565915954bcb8cc6e
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-04-07 16:36:42 -0500

    Refactored menu UI creation and animation duration:
    1) Changed menu items to represent different life options ('New Life', 'New Life as Cat', 'New Life as Dog', 'Save Life', 'Lives in Progress'),
    
    2)Created separate div elements for the main menu and upper menu section for better structure
    
    3) Adjusted animation duration to 1.5 seconds for smoother transition

--- modules/menu.js
+++ modules/menu.js
@@ -5,30 +5,37 @@ let main = document.querySelector('#main')
 
 
 export function toMenu(){
-	const menuItems = ['1','2','3','4','5','6','7']
+	const menuItems = ['New Life','New Life as Cat','New Life as Dog','Save Life','Lives in Progress']
 	let menu = document.createElement('div')
+	let mainMenu = document.createElement('div')
+	let menuUpper = document.createElement('div')
+
 	menu.classList.add('menuUI')
-	mainUI.append(menu)
+	menu.classList.add('mainMenu')
+	menuUpper.classList.add('menuUpper')
+	mainUI.append(menuUpper,menu)
 	console.log('<MENU> clicked')
 
 	let backBtn = document.createElement('img')
 	backBtn.classList.add("backBtn")
 	backBtn.src = "../assets/return.svg"
 
-	menu.appendChild(backBtn)
+	menu.append(backBtn,mainMenu)
 
 	backBtn.addEventListener('click',()=>{
 		
-		menu.style.animation = "2s menuDis"
+		menu.style.animation = "1.5s menuDis"
 		setTimeout(()=>{
 			menu.remove();
-		},2000)
+		},1500)
 	})
 	menuItems.forEach(item=>{
-		let menuList = document.createElement('li')
+		let menuList = document.createElement('div')
 		menuList.textContent = item;
+		menuList.classList.add('menuList')
+		menu.append(menuList)
+
 	})
-	menu.append(menuItems)
 
 
 
