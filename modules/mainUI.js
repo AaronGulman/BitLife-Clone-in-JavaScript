aa64166c2eff3bddd955e3c0241ac0101871b98f
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-06-25 21:56:00 -0400

    Erase the placeholder text

--- modules/mainUI.js
+++ modules/mainUI.js
@@ -160,49 +160,7 @@ function midUIfunc() {
   eventInfo.classList.add("eventInfo");
 
   eventName.textContent = `Age: ${age}`;
-  eventInfo.textContent = `${startNewLife.name} was born today. His parents are celebrating!Our policy on modern JavaScript
-
-	JavaScript is an actively evolving language and has changed greatly over the years. In particular, the 6th edition of the language (sometimes known as ECMAScript 2015 or ES6), introduced in 2015, added many new features. At the same time, to maintain backwards compatibility with older websites, old features of the language have been retained, even when they are no longer considered good practice.
-	
-	We think that the features added to JavaScript in ECMAScript 2015 and subsequent versions enable developers to write more readable, reliable, and expressive code, and that it's important to learn about them.
-	
-	The features we teach in this course are stable and have been supported by all major browsers for several years.Our policy on modern JavaScript
-
-	JavaScript is an actively evolving language and has changed greatly over the years. In particular, the 6th edition of the language (sometimes known as ECMAScript 2015 or ES6), introduced in 2015, added many new features. At the same time, to maintain backwards compatibility with older websites, old features of the language have been retained, even when they are no longer considered good practice.
-	
-	We think that the features added to JavaScript in ECMAScript 2015 and subsequent versions enable developers to write more readable, reliable, and expressive code, and that it's important to learn about them.
-	
-	The features we teach in this course are stable and have been supported by all major browsers for several years.Our policy on modern JavaScript
-
-	JavaScript is an actively evolving language and has changed greatly over the years. In particular, the 6th edition of the language (sometimes known as ECMAScript 2015 or ES6), introduced in 2015, added many new features. At the same time, to maintain backwards compatibility with older websites, old features of the language have been retained, even when they are no longer considered good practice.
-	
-	We think that the features added to JavaScript in ECMAScript 2015 and subsequent versions enable developers to write more readable, reliable, and expressive code, and that it's important to learn about them.
-	
-	The features we teach in this course are stable and have been supported by all major browsers for several years.Our policy on modern JavaScript
-
-	JavaScript is an actively evolving language and has changed greatly over the years. In particular, the 6th edition of the language (sometimes known as ECMAScript 2015 or ES6), introduced in 2015, added many new features. At the same time, to maintain backwards compatibility with older websites, old features of the language have been retained, even when they are no longer considered good practice.
-	
-	We think that the features added to JavaScript in ECMAScript 2015 and subsequent versions enable developers to write more readable, reliable, and expressive code, and that it's important to learn about them.
-	
-	The features we teach in this course are stable and have been supported by all major browsers for several years.Our policy on modern JavaScript
-
-	JavaScript is an actively evolving language and has changed greatly over the years. In particular, the 6th edition of the language (sometimes known as ECMAScript 2015 or ES6), introduced in 2015, added many new features. At the same time, to maintain backwards compatibility with older websites, old features of the language have been retained, even when they are no longer considered good practice.
-	
-	We think that the features added to JavaScript in ECMAScript 2015 and subsequent versions enable developers to write more readable, reliable, and expressive code, and that it's important to learn about them.
-	
-	The features we teach in this course are stable and have been supported by all major browsers for several years.Our policy on modern JavaScript
-
-	JavaScript is an actively evolving language and has changed greatly over the years. In particular, the 6th edition of the language (sometimes known as ECMAScript 2015 or ES6), introduced in 2015, added many new features. At the same time, to maintain backwards compatibility with older websites, old features of the language have been retained, even when they are no longer considered good practice.
-	
-	We think that the features added to JavaScript in ECMAScript 2015 and subsequent versions enable developers to write more readable, reliable, and expressive code, and that it's important to learn about them.
-	
-	The features we teach in this course are stable and have been supported by all major browsers for several years.Our policy on modern JavaScript
-
-	JavaScript is an actively evolving language and has changed greatly over the years. In particular, the 6th edition of the language (sometimes known as ECMAScript 2015 or ES6), introduced in 2015, added many new features. At the same time, to maintain backwards compatibility with older websites, old features of the language have been retained, even when they are no longer considered good practice.
-	
-	We think that the features added to JavaScript in ECMAScript 2015 and subsequent versions enable developers to write more readable, reliable, and expressive code, and that it's important to learn about them.
-	
-	The features we teach in this course are stable and have been supported by all major browsers for several years.`;
+  eventInfo.textContent = `${startNewLife.name} was born today. His parents are celebrating!`;
 
   events.append(eventName, eventInfo);
   midUI.append(events);
